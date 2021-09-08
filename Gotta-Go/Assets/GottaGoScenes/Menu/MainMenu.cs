e49d3dba923d2058de490bc6aeec699e5bf0d677
sheetalskumar <84754762+sheetalskumar@users.noreply.github.com>
2021-09-09 08:41:43 +1200

    Fucked up the PlayGameScene
    Sent yall a video on what I did before it went wrong and also explained why I did it and what I was trying to do...
    SORRY!!
    On the bright side the play button now connects to the game :D

--- Gotta-Go/Assets/GottaGoScenes/Menu/MainMenu.cs
+++ Gotta-Go/Assets/GottaGoScenes/Menu/MainMenu.cs
@@ -5,7 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
- 
- 
-    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void PlayTutorial()
+    {
+
+    }
 }
