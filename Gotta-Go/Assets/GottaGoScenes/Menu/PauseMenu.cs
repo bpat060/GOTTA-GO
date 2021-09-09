fab11871b1a6045fbbf098441f01c90a1826d3c7
sheetalskumar <84754762+sheetalskumar@users.noreply.github.com>
2021-09-10 10:25:50 +1200

    ESC for Pause Game Menu is Working
    Needs more attention to make it perfect.

--- Gotta-Go/Assets/GottaGoScenes/Menu/PauseMenu.cs
+++ Gotta-Go/Assets/GottaGoScenes/Menu/PauseMenu.cs
@@ -1,18 +1,1 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PauseMenu : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.SceneManagement;public class PauseMenu : MonoBehaviour{    public static bool GameIsPaused = false;    public GameObject pauseMenuUI;    void Update()    {        if (Input.GetKeyDown(KeyCode.Escape))        {            if (GameIsPaused)            {                Resume();            }            else            {                Pause();            }        }    }    public void Resume()    {        pauseMenuUI.SetActive(false);        Time.timeScale = 1F;        GameIsPaused = false;    }    public void Pause()    {        pauseMenuUI.SetActive(true);        Time.timeScale = 0F;        GameIsPaused = true;    }    public void LoadMenu()    {        SceneManager.LoadScene(0);    }   }
